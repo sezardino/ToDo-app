004cf20467863c1247817af1d8d1769bba0999d8
Edward <e.arekhva@gmail.com>
2020-09-06 09:09:24 +0200

    Updates

---
+++ precache-manifest.3fe14660850e71dc270f4e2883f328b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "944c543955fbfbed0d919b3e7c18ca69",
+    "url": "/ToDo-app/index.html"
+  },
+  {
+    "revision": "359eef57359f380a29a2",
+    "url": "/ToDo-app/static/css/main.52e469f6.chunk.css"
+  },
+  {
+    "revision": "be19641d3fe78d35223f",
+    "url": "/ToDo-app/static/js/2.ef8b4283.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDo-app/static/js/2.ef8b4283.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "359eef57359f380a29a2",
+    "url": "/ToDo-app/static/js/main.37c03940.chunk.js"
+  },
+  {
+    "revision": "b9efc6fc8856e03eb169",
+    "url": "/ToDo-app/static/js/runtime-main.800fd156.js"
+  }
+]);
